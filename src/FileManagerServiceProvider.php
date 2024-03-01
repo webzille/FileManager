234f68f28883a83bdf994731676c2396d9821da7
webzille <153772404+webzille@users.noreply.github.com>
2024-02-29 21:26:30 -0600

    Update FileManagerServiceProvider.php

---
+++ src/FileManagerServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Webzille\FileManager;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class FileManagerServiceProvider extends ServiceProvider
+{
+    
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/webzille-filemanager'),
+        ], 'filemanager_public');
+
+        $this->publishes([
+            __DIR__.'/../views'  => base_path('resources/views/vendor/webzille-filemanager'),
+        ], 'filemanager_view');
+        
+        $this->loadRoutesFrom(__DIR__.'/../routes/filemanager.php');
+    }
+}
