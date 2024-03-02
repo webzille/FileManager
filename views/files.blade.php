b502528d2c0405e2473d14baba5e8fb89570426a
webzille <153772404+webzille@users.noreply.github.com>
2024-03-01 18:11:39 -0600

    Update files.blade.php

--- views/files.blade.php
+++ views/files.blade.php
@@ -348,7 +348,7 @@
             const files = e.dataTransfer.files || [];
 
             uploadFiles(files);
-            unhighlight();
+            unhighlight(e);
         }
 
         function copyMainTreeViewToMoveItems() {
